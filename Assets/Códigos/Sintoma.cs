ea2f237869f5e3e6dd82817e380c1977a8e9c105
Leandro <leandro.cruz.oliveira@hotmail.com>
2019-04-01 12:55:13 -0300

    Com menu básico
    Menu principal
    Seleção e fase e troca de cenas

---
+++ Assets/Códigos/Sintoma.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Sintoma : MonoBehaviour {
+
+    public Image BarraVida;
+   // public Text Quantidade;
+    int ValorAtual = 100;
+    int Dano = 33;
+    int Energia = 66;
+
+    public void OnCollisionEnter2D(Collision2D Dano)                        //TOMOU DANO
+    {
+        if (Dano.gameObject.CompareTag("Inimigo"))              
+        {
+            Destroy(Dano.gameObject);
+            VidaPerde();
+        }
+    }
+
+    public void OnTriggerEnter2D(Collider2D Vida)                         //GANHA VIDA AO PASSAR NA BARRACA
+    {
+        if (Vida.gameObject.CompareTag("Vida"))
+        {
+            VidaGanha();
+        }
+    }
+
+
+    public void VidaPerde()
+    {
+        if (ValorAtual > 0)
+        {
+            ValorAtual -= Dano;
+            BarraVida.fillAmount = (float)ValorAtual/100;
+            //string temp = ValorAtual.ToString();
+            //Quantidade.text = temp;
+            if (ValorAtual == 1)                    //MORREU
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
+    }
+
+    public void VidaGanha()
+    {
+        if (ValorAtual < 100)
+        {
+            ValorAtual += Energia;
+            BarraVida.fillAmount = (float)ValorAtual / 100;
+            //string temp = ValorAtual.ToString();
+            //Quantidade.text = temp;
+        }
+        MaxVida();
+    }
+
+    void MaxVida()
+    {
+        if (ValorAtual > 100)
+        {
+            ValorAtual = 100;
+        }
+    }
+}
